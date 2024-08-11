fb34f712f97cc6d81e058b170be126cf67499288
pc <pc@pcui-MacBookPro.local>
2024-08-11 23:55:14 +0500

    added cors access

--- backend/src/start/modules.js
+++ backend/src/start/modules.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
 const routes = require('../routes');
@@ -5,6 +6,9 @@ const { errorHandler } = require('../middlewares/error-handler.middleware');
 
 const modules = (app, express) => {
     app.use(express.json());
+    app.use(cors({
+        origin: "*",
+    }));
     app.use(express.urlencoded({ extended: true }));
     app.use(express.static(`${process.cwd()}/uploads`))
     app.use(fileUpload());
